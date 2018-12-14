ba35655afd2ac4cc49ea460ee84a0deb65442219
Tobias B <germansuperioz@yahoo.de>
2018-12-14 22:31:12 +0100

    Added sample program

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
+// example from: https://docs.docker.com/develop/sdk/examples/#list-and-manage-containers
+// I don't know, what I want to add to this program
 func main() {
-	_ = context.Background()
-	_, err := client.NewEnvClient()
+	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+
+	// print out every container's id
+	for _, container := range containers {
+		fmt.Println(container.ID)
 	}
 }
